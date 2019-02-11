1e5f9d73c7e0949a5169c7cdeb4d59e79d9af3c3
americanman <snowboardracer7@gmail.com>
2019-02-10 20:32:06 -0500

    Init commit

---
+++ index.js
@@ -0,0 +1,42 @@
+let users = {
+  Alan: {
+    age: 27,
+    online: false
+  },
+  Vlad: {
+    age: 21,
+    online: true
+  },
+  Jeff: {
+    age: 32,
+    online: true
+  },
+  Sarah: {
+    age: 48,
+    online: false
+  },
+  Ryan: {
+    age: 19,
+    online: true
+  }
+};
+
+let countOnline = (obj) => {
+      let num = 0
+// iterate object with for...in loop
+      for (let user in obj) {
+        obj[user]['online'] ? num += 1 : null
+  }
+  return num
+}
+
+let countOffline = (obj) => {
+  let num = 0;
+// iterate object with map using Object.keys
+  Object.keys(obj).map((key) => obj[key]['online']  == false ? num+= 1 : null )
+  return num
+}
+
+console.log('users offline ' + countOffline(users))
+
+console.log('users online ' + countOnline(users));
