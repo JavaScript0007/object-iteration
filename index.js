eea4913de2153f74b4b3034de9fbe472993ae32f
americanman <snowboardracer7@gmail.com>
2019-04-02 16:16:32 -0400

    Return key value of nested object

--- index.js
+++ index.js
@@ -14,34 +14,53 @@ let users = {
   Sarah: {
     age: 48,
     online: false,
-    //here is nested object. We will find it and highlight key(name who has hidden mister: "X"
-    hiden: {
+    // this is a hidden object. We will find with with foundHidden function  
+    hidden: {
       mister: "X" 
     }
-
   },
   Ryan: {
     age: 19,
-    online: true
+    online: true,
+    hidden: {
+      mister: "X" 
+    }
   }
-};
+}
 
 let countOnline = (obj) => {
       let num = 0
 // iterate object with for...in loop
       for (let user in obj) {
-        obj[user]['online'] ? num += 1 : null
+        obj[user]['online'] ? num++ : null
   }
   return num
 }
 
-let countOffline = (obj) => {
-  let num = 0;
-// iterate object with map using Object.keys
-  Object.keys(obj).map((key) => obj[key]['online']  == false ? num+= 1 : null )
-  return num
+
+let off = 0
+
+let countOff = (obj) => {
+  for(person in obj) {
+    obj[person]['online'] != true ? off++ : null
+  }
+  return off
 }
 
-console.log('users offline ' + countOffline(users))
+const foundHidden = (obj) => {
+  let result = ''
+  const keysArr = Object.keys(obj)
+  // mapping over the array of object's keys
+  keysArr.map((item, index) => {
+  // we need check here to not break the loop when hidden is undefined
+    obj[item]["hidden"] !== undefined ?
+  // when it is defined we return value from the array of keys with current index 
+      (obj[item]["hidden"]['mister'] == 'X' ? result += " found in " + Object.getOwnPropertyNames(obj)[index] : null) :
+      null
+  })
+  return result
+}
+console.log(foundHidden(users))
 
-console.log('users online ' + countOnline(users));
+console.log(countOnline(users))
+console.log(countOff(users))
